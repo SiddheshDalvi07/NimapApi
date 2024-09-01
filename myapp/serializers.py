5602714e7087c74916a76b3b2f09faf01733672a
SiddheshDalvi07 <dalvisiddhesh3@gmail.com>
2024-09-01 19:07:29 +0530

    changes

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -18,13 +18,13 @@ class ProjectListSerializer(serializers.ModelSerializer):
 
 class ProjectDetailSerializer(serializers.ModelSerializer):
     project_name = serializers.CharField(source='name')
-    client_name = serializers.CharField(source='client.name', read_only=True)
+    client = serializers.CharField(source='client.name', read_only=True)
     users = UserSerializer(many=True, read_only=True)
     created_by = serializers.CharField(source='created_by.username', read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'project_name', 'client_name', 'users', 'created_at', 'created_by']
+        fields = ['id', 'project_name', 'client', 'users', 'created_at', 'created_by']
 
 
 class ProjectShortSerializer(serializers.ModelSerializer): 
